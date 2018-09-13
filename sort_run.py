370483db19c0c9f9a340d7459df811b332139c94
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2018-09-13 16:52:51 +1000

    support custom sorting

--- sort_run.py
+++ sort_run.py
@@ -1,44 +1,56 @@
-from operator import itemgetter
+#!/usr/bin/env python3
 import argparse
 import subprocess
 import sys
 import pandas as pd
-from io import BytesIO
+from pathlib import Path
+import itertools
 
 
 def eprint(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr, flush=True)
 
 
-def trec_eval_summary(measure, qrel, run_file):
-    args = ['trec_eval', '-m', measure, qrel, run_file]
-    proc = subprocess.run(args, stdout=subprocess.PIPE)
+def trec_eval(measure, qrel_path, run_path):
+    trec_eval = str(Path(__file__).resolve().with_name('trec_eval'))
+    args = [trec_eval, '-m', measure, qrel_path, run_path]
+    eprint(' '.join(args))
+    proc = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    aggregated = {}
+    for line in proc.stdout.decode('utf-8').splitlines():
+        measure, qno, value = line.split()
+        aggregated[measure] = value
 
-    output = proc.stdout.decode('utf-8')
-
-    parts = output.split()
-    return float(parts[2])
+    return aggregated
 
 
 def gdeval(k, qrel_path, run_path):
-    gdeval = 'gdeval'
+    gdeval = str(Path(__file__).resolve().with_name('gdeval.pl'))
     args = [gdeval, '-k', str(k), qrel_path, run_path]
     eprint(' '.join(args))
     proc = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    df = pd.read_csv(BytesIO(proc.stdout))
-    qno_results = {}
-    aggregated = {}
-    for _, row in df.iterrows():
-        if row['topic'] == 'amean':
-            aggregated[df.columns[2]] = row[df.columns[2]]
-            aggregated[df.columns[3]] = row[df.columns[3]]
-            continue
-        qno_results.setdefault(row['topic'],
-                               {})[df.columns[2]] = row[df.columns[2]]
-        qno_results.setdefault(row['topic'],
-                               {})[df.columns[3]] = row[df.columns[3]]
+    lines = proc.stdout.decode('utf-8').splitlines()
+    _, _, ndcg, err = lines[0].split(',')
 
-    return aggregated, qno_results
+    aggregated = {}
+    for line in lines[1:]:
+        _, _, ndcg_value, err_value = line.split(',')
+        aggregated[ndcg] = ndcg_value
+        aggregated[err] = err_value
+
+    return aggregated
+
+
+def gdeval_all(qrel_path, run_path):
+    aggregated = {}
+    for k in [5, 10, 15, 20, 30, 100, 200, 500, 1000]:
+        agg = gdeval(k, qrel_path, run_path)
+        aggregated.update(agg)
+    return aggregated
+
+
+def split_comma(s):
+    return s.split(',')
 
 
 def parse_args():
@@ -48,12 +60,18 @@ def parse_args():
     parser.add_argument(
         '--measure',
         required=True,
+        type=split_comma,
         help='Measure. For trec ndcg, use ndcg_cut.20; '
         'for gdeval ndcg, use gdeval@20 ...')
 
-    parser.add_argument('--qrel', required=True, help='qrel')
+    parser.add_argument(
+        '--sort-by',
+        '-s',
+        help='Sort by, like gdeval-ndcg@20, otherwise the first measure')
 
-    parser.add_argument('run', nargs='+', help='run files')
+    parser.add_argument('qrel', metavar='QREL', help='qrel')
+
+    parser.add_argument('run', nargs='+', metavar='RUN', help='run files')
 
     args = parser.parse_args()
 
@@ -63,25 +81,41 @@ def parse_args():
 def main():
     args = parse_args()
 
-    count = len(args.run)
-    results = []
-    for i, filename in enumerate(args.run, 1):
-        try:
-            if args.measure.startswith('gdeval'):
-                k = args.measure.split('@')[1]
-                result, _ = gdeval(k, args.qrel, filename)
-                result = result['ndcg@' + k]
+    count = len(args.run) * len(args.measure)
+    results = {}
+    measures = []
+    for i, (filename, measure) in enumerate(
+            itertools.product(args.run, args.measure)):
+        results.setdefault(filename, [])
+        if measure.startswith('gdeval'):
+            if '@' in measure:
+                k = measure.split('@')[1]
+                result = gdeval(k, args.qrel, filename)
             else:
-                result = trec_eval_summary(args.measure, args.qrel, filename)
-            results.append([filename, result])
-        except Exception as e:
-            print(e.description(), file=sys.stderr)
+                result = gdeval_all(args.qrel, filename)
+        else:
+            result = trec_eval(measure, args.qrel, filename)
 
-        print('{}/{}\r'.format(i, count), end='', flush=True, file=sys.stderr)
-    print('')
-    results.sort(key=itemgetter(1), reverse=True)
-    for (filename, result) in results:
-        print('{:.3f} {}'.format(result, filename))
+        for m, value in result.items():
+            m = m.replace('_cut_', '@')
+            m = m.replace('_', '@')
+            m = 'GDEVAL-' + m if measure.startswith('gdeval') else m
+            m = 'TREC-' + m if measure.startswith('ndcg') else m
+            m = m.upper()
+            if m not in measures:
+                measures.append(m)
+            results[filename].append(value)
+        # eprint(m, result)
+
+        eprint('{}/{}\r'.format(i, count), end='')
+
+    results = [[f] + values for f, values in results.items()]
+    df = pd.DataFrame(results, columns=['FILE'] + measures)
+    if args.sort_by in df.columns:
+        df = df.sort_values(by=args.sort_by.upper(), ascending=False)
+    else:
+        df = df.sort_values(by=df.columns[1], ascending=False)
+    print(df.to_latex(index=False, float_format=lambda f: '{:.3f}'.format(f)))
 
 
 if __name__ == '__main__':
