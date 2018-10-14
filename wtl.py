6cc43d1b7218d5d727c75660a86dabcb6e009abe
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2018-10-14 23:55:04 +1100

    add test of queries at quartiles

--- wtl.py
+++ wtl.py
@@ -3,10 +3,20 @@ from eval_run import eval_run
 import sys
 import argparse
 from operator import itemgetter
+from itertools import zip_longest, chain
+from numpy import array_split
+import pandas as pd
 
 
 def eprint(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr, flush=True)
+
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
 
 def parse_args():
@@ -23,9 +33,9 @@ def parse_args():
 
     parser.add_argument('qrel', metavar='QREL', help='qrel')
 
-    parser.add_argument('run1')
+    parser.add_argument('base')
 
-    parser.add_argument('run2')
+    parser.add_argument('comparison')
 
     args = parser.parse_args()
 
@@ -35,32 +45,31 @@ def parse_args():
 def main():
     args = parse_args()
 
-    _, result1 = eval_run('gdeval@20', args.qrel, args.run1)
+    _, result1 = eval_run('gdeval@20', args.qrel, args.base)
     result1 = {qno: m['GDEVAL-NDCG@20'] for qno, m in result1.items()}
 
-    sort = sorted(result1.items(), key=itemgetter(1))
-    sort = [q for q, _ in sort[:50]]
-
-    _, result2 = eval_run('gdeval@20', args.qrel, args.run2)
+    _, result2 = eval_run('gdeval@20', args.qrel, args.comparison)
     result2 = {qno: m['GDEVAL-NDCG@20'] for qno, m in result2.items()}
 
-    queries = sort
-    # queries = set(result1.keys()) | set(result2.keys())
-    win = 0
-    tie = 0
-    loss = 0
-    for qno in queries:
-        s1 = result1.get(qno, 0.0)
-        s2 = result2.get(qno, 0.0)
-        if s2 > s1 and (s2 - s1) > s1 * args.threshold:
-            win += 1
-        elif s2 < s1 and (s1 - s2) > s1 * args.threshold:
-            loss += 1
-        else:
-            tie += 1
+    sorted_by_base = sorted(result1.items(), key=itemgetter(1))
+    names = ['0-100', '0-25', '25-50', '50-75', '75-100']
+    r = []
+    for n, g in zip(names,
+                    chain([sorted_by_base], array_split(sorted_by_base, 4))):
+        win, tie, loss = 0, 0, 0
+        for qno, _ in g:
+            s1 = result1.get(qno, 0.0)
+            s2 = result2.get(qno, 0.0)
+            if s2 > s1 and (s2 - s1) > s1 * args.threshold:
+                win += 1
+            elif s2 < s1 and (s1 - s2) > s1 * args.threshold:
+                loss += 1
+            else:
+                tie += 1
+        r.append((n, win, tie, loss))
 
-    print('win,tie,loss')
-    print('{},{},{}'.format(win, tie, loss))
+    df = pd.DataFrame(r, columns=['range', 'win', 'tie', 'loss'])
+    print(df.to_latex(index=False))
 
 
 if __name__ == '__main__':
