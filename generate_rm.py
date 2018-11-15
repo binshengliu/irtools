a5d798cbb00961413d40b44f0594799073cf0b37
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2018-11-15 17:16:57 +1100

    restore query likelihood for relevance modeling

--- generate_rm.py
+++ generate_rm.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import sys
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import itertools
+import lxml.etree as ET
+from tempfile import NamedTemporaryFile
 
 
 def eprint(*args, **kwargs):
@@ -41,6 +43,32 @@ def rmodels_mp(pool, index, runfile, docs, fields, template):
         eprint('{}'.format(path))
 
 
+def load_param(path):
+    tree = ET.parse(str(path), ET.XMLParser(remove_blank_text=True))
+    root = tree.getroot()
+    length_dict = {}
+    for query in root.findall('query'):
+        text = query.find('text').text
+        number = query.find('number').text
+        length = len(text.split())
+        length_dict[number] = length
+    return length_dict
+
+
+def convert_ce_to_ql(param_file, run_file):
+    length_dict = load_param(param_file)
+    converted = []
+    with open(run_file, 'r') as f:
+        for line in f:
+            # qno, q0, docno, rank, score, runid = line.split()
+            splits = line.split()
+            score = float(splits[4]) * length_dict[splits[0]]
+            splits[4] = '{}'.format(score)
+            converted.append(' '.join(splits) + '\n')
+
+    return converted
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Generate relevance models.')
 
@@ -50,6 +78,7 @@ def parse_args():
     def comma_str(s):
         return [str(t) for t in s.split(',')]
 
+    parser.add_argument('--param', type=Path, required=True)
     parser.add_argument('--run', type=Path, required=True)
     parser.add_argument('--index', type=Path, required=True)
     parser.add_argument('--fields', type=comma_str, default=['all'])
@@ -61,16 +90,14 @@ def parse_args():
     return args
 
 
-def main_mp(pool):
+def main(pool):
     args = parse_args()
 
-    rmodels_mp(pool, args.index, args.run, args.docs, args.fields,
-               args.template)
-
-
-def main():
-    with ProcessPoolExecutor() as pool:
-        main_mp(pool)
+    with ProcessPoolExecutor() as pool, NamedTemporaryFile(mode='w') as f:
+        converted = convert_ce_to_ql(args.param, args.run)
+        f.writelines(converted)
+        rmodels_mp(pool, args.index, f.name, args.docs, args.fields,
+                   args.template)
 
 
 if __name__ == '__main__':
