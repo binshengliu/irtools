476723503677b0f6d4a7d3112e4a65ed0317ce4b
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2019-06-12 17:17:50 +1000

    support distributed

--- indri.py
+++ indri.py
@@ -1,6 +1,7 @@
 from concurrent.futures import ProcessPoolExecutor as Pool
 import subprocess
 import sys
+from dask.distributed import Client
 
 
 def eprint(*args, **kwargs):
@@ -8,10 +9,11 @@ def eprint(*args, **kwargs):
 
 
 class IndriRunQuery:
-    def __init__(self, path, index):
+    def __init__(self, path, index, scheduler=None):
         if not path:
             path = 'IndriRunQuery'
         self._args = [path, '-index=' + index.strip(), '-trecFormat=True']
+        self._scheduler = scheduler
 
     def _run_single(self, qno, query, extra=None):
         indri_args = self._args + [
@@ -22,11 +24,13 @@ class IndriRunQuery:
         if extra:
             indri_args.extend(extra)
 
-        proc = subprocess.Popen(
-            indri_args, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        proc = subprocess.Popen(indri_args,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.DEVNULL,
+                                encoding='ascii',
+                                errors='ignore')
         output = []
         for line in proc.stdout:
-            line = line.decode('utf-8')
             if 'EXCEPTION' in line:
                 eprint('EXCEPTION: {} {} {}'.format(qno, query, line))
                 raise Exception('EXCEPTION: {} {} {}'.format(qno, query, line))
@@ -40,4 +44,16 @@ class IndriRunQuery:
         with Pool() as pool:
             output = list(
                 pool.map(self._run_single, qnos, queries, [extra] * count))
+        return output
+
+    def run_distributed(self, qnos, queries, extra=None):
+        '''Set up a cluster first:
+        dask-scheduler
+        env PYTHONPATH=/research/remote/petabyte/users/binsheng/trec_tools/ dask-worker segsresap10:8786 --nprocs 50 --nthreads 1 --memory-limit 0 --name segsresap10
+        env PYTHONPATH=/research/remote/petabyte/users/binsheng/trec_tools/ dask-worker segsresap10:8786 --nprocs 50 --nthreads 1 --memory-limit 0 --name segsresap09
+        '''
+        client = Client(self._scheduler)
+        count = len(qnos)
+        futures = client.map(self._run_single, qnos, queries, [extra] * count)
+        output = [f.result() for f in futures]
         return output
