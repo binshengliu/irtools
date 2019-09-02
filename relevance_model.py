9a920edb305b7293016404a2a7fe622c2c0867f9
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2019-09-02 11:45:00 +1000

    add TrecRelevanceModels

--- relevance_model.py
+++ relevance_model.py
@@ -1,5 +1,5 @@
 from krovetzstemmer import Stemmer
-from collections import Counter
+from collections import Counter, OrderedDict
 from operator import itemgetter
 import math
 from stopwords import stopwords
@@ -66,14 +66,94 @@ class RelevanceModel:
 
         self._rm = rm
 
-    def indri_rm1(self, nterms=None):
+    def indri_rm1(self, nterms=None, whitelist=None):
         rm = list(sorted(self._rm.items(), reverse=True, key=itemgetter(1)))
+        if whitelist:
+            rm = list(filter(lambda x: x[0] in whitelist, rm))
         rm = rm[:nterms]
         rm = ['{:f} {}'.format(pair[1], pair[0]) for pair in rm]
         rm = '#weight({})'.format(' '.join(rm))
         return rm
 
-    def bag_of_words(self, nterms=None):
+    def bag_of_words(self, nterms=None, whitelist=None):
         rm = list(sorted(self._rm.keys(), reverse=True, key=self._rm.get))
+        if whitelist:
+            rm = list(filter(whitelist.__contains__, rm))
         rm = ' '.join(rm[:nterms])
         return rm
+
+
+class TrecRelevanceModels:
+    def __init__(self,
+                 *,
+                 trec_output,
+                 doc_texts,
+                 stem=False,
+                 remove_stop=False):
+        """scores: a list of scores, can be log value or probabilities
+        terms: [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h']] or
+               ['a b c d', 'e f g h'] or
+               [[['a', 0.4], ['b', 0.3], ['c', 0.2]], [['e', 0.5], ['f', 0.5]]]
+        """
+        self._stem = stem
+        self._remove_stop = remove_stop
+        self._doc_texts = {}
+        self._qno_docs = OrderedDict()
+        self._qno_scores = OrderedDict()
+        for line in trec_output.splitlines():
+            qno, _, docno, _, score, _ = line.split()
+            self._qno_docs.setdefault(qno, []).append(docno)
+            self._qno_scores.setdefault(qno, []).append(float(score))
+            self._doc_texts[docno] = doc_texts[docno]
+
+    def _build_rms(self, ndocs):
+        ndocs_dict = self._build_qno_map(ndocs)
+        qno_docs = OrderedDict(
+            map(lambda k, v: (k, v[:ndocs_dict[k]]), self._qno_docs.keys(),
+                self._qno_docs.values()))
+        qno_scores = OrderedDict(
+            map(lambda k, v: (k, v[:ndocs_dict[k]]), self._qno_scores.keys(),
+                self._qno_scores.values()))
+
+        rms = OrderedDict()
+        for qno, docs in qno_docs.items():
+            terms = [self._doc_texts[d] for d in docs]
+            scores = qno_scores[qno]
+
+            rm = RelevanceModel(terms=terms,
+                                scores=scores,
+                                stem=self._stem,
+                                remove_stop=self._remove_stop)
+            rms[qno] = rm
+        return rms
+
+    def _build_qno_map(self, to_build):
+        if isinstance(to_build, dict):
+            qno_map = to_build
+        elif isinstance(to_build, list):
+            qno_map = dict(zip(self._qno_docs.keys(), to_build))
+        else:
+            qno_map = dict(
+                zip(self._qno_docs.keys(),
+                    [to_build] * len(self._qno_docs.keys())))
+        return qno_map
+
+    def indri_rm1(self, *, ndocs=None, nterms=None, whitelist=None):
+        rms = self._build_rms(ndocs)
+        terms_dict = self._build_qno_map(nterms)
+        white_dict = self._build_qno_map(whitelist)
+        queries = [
+            rm.indri_rm1(terms_dict[qno], white_dict[qno])
+            for qno, rm in rms.items()
+        ]
+        return list(rms.keys()), queries
+
+    def bag_of_words(self, *, ndocs=None, nterms=None, whitelist=None):
+        rms = self._build_rms(ndocs)
+        terms_dict = self._build_qno_map(nterms)
+        white_dict = self._build_qno_map(whitelist)
+        queries = [
+            rm.bag_of_words(terms_dict[qno], white_dict[qno])
+            for qno, rm in rms.items()
+        ]
+        return list(rms.keys()), queries
