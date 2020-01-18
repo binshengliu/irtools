0fe6d74cce07cb0923bfb2f308cfd6a9db041085
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-01-18 15:08:30 +1100

    add setup.py MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='irtools',
+    version='1.0',
+    description='Utilities for IR research',
+    author='Binsheng Liu',
+    author_email='liubinsheng@gmail.com',
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    include_package_data=True,
+    install_requires=[
+        'unidecode', 'tqdm', 'scipy', 'numpy', 'more_itertools', 'pandas'
+    ],
+)
