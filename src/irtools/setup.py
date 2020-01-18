36351a5cc0752183b1c2c7c6c4e33dff9ab4e885
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-01-18 15:08:02 +1100

    update location

--- src/irtools/setup.py
+++ src/irtools/setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='ir_tools',
+    name='irtools',
     version='1.0',
     description='Utilities for IR research',
     author='Binsheng Liu',
     author_email='liubinsheng@gmail.com',
-    packages=['ir_tools'],
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     install_requires=[
         'unidecode', 'tqdm', 'scipy', 'numpy', 'more_itertools', 'pandas'
     ],
