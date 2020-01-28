1793218997d7a7223e897a31dbf7624adaf51e95
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-01-28 16:14:17 +1100

    support gpt2 and openai-gpt

--- src/irtools/perplexity.py
+++ src/irtools/perplexity.py
@@ -1,76 +1,86 @@
 #!/usr/bin/env python3
-from transformers import OpenAIGPTLMHeadModel as Model
-from transformers import OpenAIGPTTokenizer as Tokenizer
-from concurrent.futures import ProcessPoolExecutor as Pool
+from transformers import (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer,
+                          GPT2LMHeadModel, GPT2Tokenizer)
+from multiprocessing import Process, Queue
 from more_itertools import chunked
-from itertools import chain, count, repeat
-from tqdm import tqdm
+from tqdm import trange
 import argparse
 import GPUtil
 import torch
 import math
 import sys
 
+MODEL_CLASSES = {
+    "gpt2": (GPT2LMHeadModel, GPT2Tokenizer),
+    "openai-gpt": (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer),
+}
+
 
 def eprint(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr, flush=True)
 
 
-def score(sentence, gpu=0):
+def worker(batch_sent, model_type, model_name, gpu, q):
     # Load pre-trained model (weights)
-    model = Model.from_pretrained('openai-gpt').cuda(gpu)
+    model_class, tokenizer_class = MODEL_CLASSES[model_type]
+    model = model_class.from_pretrained(model_name).cuda(gpu)
     model.eval()
     # Load pre-trained model tokenizer (vocabulary)
-    tokenizer = Tokenizer.from_pretrained('openai-gpt')
+    tokenizer = tokenizer_class.from_pretrained(model_name)
 
-    results = []
-    for sent in tqdm(
-            sentence,
-            total=len(sentence),
-            position=gpu,
-            leave=False,
-            desc=str(gpu)):
+    for id, sent in batch_sent:
         try:
-            if len(sent.split()) == 1:
-                sent = sent + ' ' + sent
             nwords = len(sent.split())
             tensor_input = torch.tensor([tokenizer.encode(sent)]).cuda(gpu)
             loss = model(input_ids=tensor_input, labels=tensor_input)
             ppl = math.exp(loss[0].item())
-            results.append((ppl, ppl * nwords, ppl / nwords))
+            q.put((id, ppl, ppl * nwords, ppl / nwords))
         except Exception:
-            print('ERROR {}'.format(sent), flush=True)
+            q.put((id, None, None, None))
             eprint('ERROR {}'.format(sent))
-    return results
+    return
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--query', required=True, type=argparse.FileType('r'))
+    parser.add_argument('--model-type', default='openai-gpt')
+    parser.add_argument('--model-name', default='openai-gpt')
     return parser.parse_args()
 
 
-def perplexity(queries):
+def perplexity(queries, model_type='openai-gpt', model_name='openai-gpt'):
     deviceIDs = GPUtil.getAvailable(
-        order='first', limit=4, maxLoad=0.5, maxMemory=0.5)
+        order='first', limit=8, maxLoad=0.5, maxMemory=0.5)
     nproc = len(deviceIDs)
     if nproc == 0:
         raise ValueError('No available GPU')
 
+    queries = list(enumerate(queries))
     total = len(queries)
-    chunksize = math.ceil(total / nproc)
-    queries = chunked(queries, chunksize)
-    with Pool(nproc) as pool:
-        result = pool.map(score, queries, deviceIDs)
-        result = list(chain.from_iterable(result))
-    return result
+    queries = list(chunked(queries, math.ceil(total / nproc)))
+    procs = []
+    q = Queue()
+    for gpu, batch_sent in zip(deviceIDs, queries):
+        proc = Process(
+            target=worker, args=(batch_sent, model_type, model_name, gpu, q))
+        proc.start()
+        procs.append(proc)
+    results = [None] * total
+    for _ in trange(total):
+        id, *ppls = q.get()
+        results[id] = ppls
+    for proc in procs:
+        proc.join()
+
+    return results
 
 
 def main():
     args = parse_arguments()
 
     queries = args.query.read().splitlines()
-    result = perplexity(queries)
+    result = perplexity(queries, args.model_type, args.model_name)
     print(''.join(['{}\t{}\t{}\n'.format(*x) for x in result]), end='')
 
 
