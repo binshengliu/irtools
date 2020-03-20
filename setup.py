fb671321cf199e29054cf7c1d1ae561588ff17a2
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-03-20 19:21:06 +1100

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='irtools',
-    version='1.0.2',
+    version='1.0.3',
     description='Utilities for IR research',
     author='Binsheng Liu',
     author_email='liubinsheng@gmail.com',
