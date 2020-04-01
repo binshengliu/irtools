feb31eb0a602d2948f0cd588a8c25ee742f083ba
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-04-01 13:24:45 +1100

    label.py supporting adding qrels

--- scripts/label.py
+++ scripts/label.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import OrderedDict
 import argparse
 import sys
 
@@ -22,7 +23,38 @@ def parse_arguments():
         default=sys.stdout,
         help='default stdout')
 
+    parser.add_argument(
+        '--append-missing-relevant',
+        action='store_true',
+        help='Append relevant documents to form training data. '
+        'This implies output in a three column format: qno\tdno\trel')
+
     return parser.parse_args()
+
+
+def parse_qrel_line(line):
+    splits = line.split()
+    if len(splits) == 2:
+        qno, dno, rel = *splits, 1
+    elif len(splits) == 3:
+        qno, dno, rel = splits
+    elif len(splits) == 4:
+        # Trec format
+        qno, _, dno, rel = splits
+    return qno, dno, int(rel)
+
+
+def parse_run_line(line):
+    splits = line.split()
+    if len(splits) == 2:
+        qno, dno = splits
+    elif len(splits) == 3:
+        qno, dno, _ = splits
+    elif len(splits) == 6:
+        qno, _, dno, *_ = splits
+    else:
+        assert False, f'Unknown format {len(splits)} fields found'
+    return qno, dno
 
 
 def main():
@@ -30,30 +62,29 @@ def main():
 
     qrels = {}
     for line in args.qrels:
-        splits = line.split()
-        if len(splits) == 2:
-            qno, dno, rel = *splits, 1
-        elif len(splits) == 3:
-            qno, dno, rel = splits
-        elif len(splits) == 4:
-            # Trec format
-            qno, _, dno, rel = splits
-        qrels.setdefault(qno, {})[dno] = int(rel)
+        qno, dno, rel = parse_qrel_line(line)
+        qrels.setdefault(qno, {})[dno] = rel
 
+    delimeter = '\t'
+    data = OrderedDict()
     for line in args.input:
-        delimeter = ' ' if ' ' in line else '\t'
-        splits = line.split()
-        if len(splits) == 2:
-            qno, dno = splits
-        elif len(splits) == 3:
-            qno, dno, _ = splits
-        elif len(splits) == 6:
-            qno, _, dno, *_ = splits
-        else:
-            assert False, f'Unknown format {len(splits)} fields found'
-        rel = qrels.get(qno, {}).get(dno, 0)
+        delimeter = '\t' if '\t' in line else ' '
+        qno, dno = parse_run_line(line)
+        rel = qrels.get(qno, {}).pop(dno, 0)
 
-        args.output.write(line.rstrip('\n') + f'{delimeter}{rel}\n')
+        if args.append_missing_relevant:
+            data.setdefault(qno, []).append((dno, rel))
+        else:
+            args.output.write(line.rstrip('\n') + f'{delimeter}{rel}\n')
+
+    if args.append_missing_relevant:
+        for qno in data.keys():
+            for dno, rel in qrels.get(qno, {}).items():
+                data[qno].append((dno, rel))
+
+        for qno, dno_rel in data.items():
+            for dno, rel in dno_rel:
+                args.output.write(delimeter.join([qno, dno, str(rel)]) + '\n')
 
 
 if __name__ == '__main__':
