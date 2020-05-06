974e095044c991a0b157c7e3a08905097ba1bb56
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-05-06 12:05:33 +1000

    allow specifying filenames for ans2trec.py and trec2ans.py

--- scripts/ans2trec.py
+++ scripts/ans2trec.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
+import argparse
 import sys
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument(
+        "-i", "--input", type=argparse.FileType("r"), default=sys.stdin, help=""
+    )
+    parser.add_argument(
+        "-o", "--output", type=argparse.FileType("w"), default=sys.stdout, help=""
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    for line in sys.stdin:
+    args = parse_arguments()
+    for line in args.input:
         qno, dno, rank = line.strip().split()
-        score = 1.0 / int(rank)
-        print(f'{qno} Q0 {dno} {rank} {score} ANSERINI')
+        score = -int(rank)
+        args.output.write(f"{qno} Q0 {dno} {rank} {score} ANSERINI\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- scripts/trec2ans.py
+++ scripts/trec2ans.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
+import argparse
 import sys
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument(
+        "-i", "--input", type=argparse.FileType("r"), default=sys.stdin, help=""
+    )
+    parser.add_argument(
+        "-o", "--output", type=argparse.FileType("w"), default=sys.stdout, help=""
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    for line in sys.stdin:
+    args = parse_arguments()
+    for line in args.input:
         qno, _, dno, rank, _, _ = line.strip().split()
-        print(f'{qno} {dno} {rank}')
+        args.output.write(f"{qno}\t{dno}\t{rank}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
