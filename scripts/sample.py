83c758656827b0518ee54ab7bc85f5dc59f616c2
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-05-06 12:37:47 +1000

    support front and back selection

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import sys
-
-from tqdm import tqdm
 import argparse
+import sys
+from collections import OrderedDict
+
 import numpy as np
+from tqdm import tqdm
 
 
 def parse_arguments():
@@ -13,58 +14,70 @@ def parse_arguments():
             raise argparse.ArgumentTypeError("Field starts from 1.")
         return x - 1
 
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description="")
 
     parser.add_argument(
-        '-i',
-        '--input',
-        type=argparse.FileType('r'),
-        default=sys.stdin,
-        help='')
+        "-i", "--input", type=argparse.FileType("r"), default=sys.stdin, help=""
+    )
+
+    parser.add_argument("-d", "--delimiter", default="\t", help="default to \\t")
+
+    parser.add_argument("--mode", choices=["key", "value", "both"], required=True)
+    parser.add_argument("--key-field", type=to_zero_base, default=0)
+    parser.add_argument("--key-num", type=int, default=1)
+    parser.add_argument("--value-num", type=int, default=1)
+    parser.add_argument(
+        "--selection", choices=["front", "back", "random"], required=True
+    )
 
     parser.add_argument(
-        '-d', '--delimiter', default='\t', help='default to \\t')
-
-    parser.add_argument(
-        '--mode', choices=['key', 'value', 'both'], required=True)
-    parser.add_argument('--key-field', type=to_zero_base, default=0)
-    parser.add_argument('--key-num', type=int, default=1)
-    parser.add_argument('--value-num', type=int, default=1)
-
-    parser.add_argument(
-        '-o',
-        '--output',
-        type=argparse.FileType('w'),
-        default=sys.stdout,
-        help='')
+        "-o", "--output", type=argparse.FileType("w"), default=sys.stdout, help=""
+    )
 
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    data = {}
+    data = OrderedDict()
     for line in args.input:
         if not line.strip():
             continue
         splits = line.strip().split(args.delimiter)
         data.setdefault(splits[args.key_field], []).append(line)
 
-    if args.mode in ['key', 'both']:
+    if args.mode in ["key", "both"]:
         keys = list(data.keys())
-        keys = np.random.choice(
-            keys, min(len(keys), args.key_num), replace=False)
-        data = {k: data[k] for k in keys}
+        num = min(len(keys), args.key_num)
+        if args.selection == "random":
+            keys = np.random.choice(keys, num, replace=False)
+        elif args.selection == "front":
+            keys = keys[:num]
+        elif args.selection == "back":
+            keys = keys[-num:]
+        else:
+            assert False
+        keys = set(keys)
+        data = OrderedDict([(k, v) for k, v in data.items() if k in keys])
 
-    if args.mode in ['value', 'both']:
-        data = {
-            k: np.random.choice(v, min(len(v), args.value_num), replace=False)
-            for k, v in tqdm(data.items(), desc='Sampling values')
-        }
+    if args.mode in ["value", "both"]:
+        outputs = OrderedDict()
+        for k, v in tqdm(data.items(), desc="Sampling values"):
+            num = min(len(v), args.value_num)
+            if args.selection == "random":
+                v = np.random.choice(v, num, replace=False)
+            elif args.selection == "front":
+                v = v[:num]
+            elif args.selection == "back":
+                v = v[-num:]
+            else:
+                assert False
+            outputs[k] = v
+        data = outputs
 
     for values in data.values():
         args.output.writelines(list(values))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
