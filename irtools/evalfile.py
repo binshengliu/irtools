2d657551eb1b35103f26899ac95f24c6a60d2197
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-05-29 11:26:06 +1000

    support multiple metrics of trec_eval output

--- irtools/evalfile.py
+++ irtools/evalfile.py
@@ -2,34 +2,65 @@ from __future__ import annotations
 
 import os
 from collections import OrderedDict
-from typing import AnyStr, Iterator, Tuple
+from typing import AnyStr, Dict, Iterator, List, Optional, Tuple
 
-import numpy as np
-from more_itertools import with_iter
+from more_itertools import first, with_iter
 
 
 class TrecEval:
     def __init__(self, path: os.PathLike[AnyStr], default: float = 0.0):
         lines = [line.strip("\n").split() for line in with_iter(open(path, "r"))]
         assert all(len(x) == 3 for x in lines), "Malformed eval file"
-        self._metric = lines[0][0]
-        self._qno_map = OrderedDict((x[1], float(x[2])) for x in lines)
+
+        self._content: Dict[str, Dict[str, float]] = OrderedDict()
+        self._agg: Dict[str, float] = OrderedDict()
+        for metric, qid, value in lines:
+            self._content.setdefault(metric, OrderedDict())
+            if qid == "all":
+                self._agg[metric] = float(value)
+            else:
+                self._content[metric][qid] = float(value)
+
         self._default = default
-        agg = round(np.mean(list(self._qno_map.values())), 4)
-        if "all" in self._qno_map:
-            self._agg = self._qno_map.pop("all")
-            assert np.isclose(self._agg, agg), "The aggregated value is incorrect"
-        else:
-            self._agg = agg
 
-    def __iter__(self) -> Iterator[Tuple[str, float]]:
-        return iter(self._qno_map.items())
-
-    def __getitem__(self, qno: str) -> float:
-        return self._qno_map.get(qno, self._default)
-
-    def get(self, qno: str) -> float:
-        return self._qno_map.get(qno, self._default)
+        # Check the qids are consistent across metrics
+        first_qids = set(first(self._content.values()).keys())
+        assert all(set(x.keys()) == first_qids for x in self._content.values())
 
     def __len__(self) -> int:
-        return len(self._qno_map)
+        return sum(len(x) for x in self._content.values())
+
+    def __iter__(self) -> Iterator[Tuple[str, str, float]]:
+        for metric, qid_values in self._content.items():
+            for qid, value in qid_values.items():
+                yield metric, qid, value
+
+    def metrics(self) -> List[str]:
+        return list(self._content.keys())
+
+    def qids(self) -> List[str]:
+        return list(first(self._content.values()).keys())
+
+    def qid_value(self, qid: str, metric: Optional[str] = None) -> float:
+        if metric is None:
+            assert len(self._content) == 1, "metric can only be omitted when unique."
+            return first(self._content.values()).get(qid, self._default)
+        else:
+            return self._content[metric].get(qid, self._default)
+
+    def qid_values(self, qid: str) -> Dict[str, float]:
+        return {metric: values[qid] for metric, values in self._content.items()}
+
+    def agg_value(self, metric: Optional[str] = None) -> float:
+        if metric is None:
+            assert len(self._agg) == 1, "metric can only be omitted when unique"
+            return first(self._agg.values())
+        else:
+            return self._agg[metric]
+
+    def metric_values(self, metric: Optional[str] = None) -> List[float]:
+        if metric is None:
+            assert len(self._content) == 1, "metric can only be omitted when unique"
+            return list(first(self._content.values()).values())
+        else:
+            return list(self._content[metric].values())
