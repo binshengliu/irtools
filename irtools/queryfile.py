e2a7860c2f813ba9b90e63a16bde696dcec767fb
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-06-08 12:06:44 +1000

    add load to TrecQuery

--- irtools/queryfile.py
+++ irtools/queryfile.py
@@ -8,15 +8,19 @@ from more_itertools import with_iter
 
 
 class TrecQuery:
-    def __init__(
-        self,
+    def __init__(self, qno_map: OrderedDict[str, str]):
+        self._qno_map = qno_map
+
+    @staticmethod
+    def load(
         path: os.PathLike[AnyStr],
         sep: str = "\t",
         qid_field: int = 0,
         query_field: int = 1,
-    ):
+    ) -> TrecQuery:
         content = [line.strip("\n").split(sep) for line in with_iter(open(path, "r"))]
-        self._qno_map = OrderedDict([(x[qid_field], x[query_field]) for x in content])
+        qno_map = OrderedDict([(x[qid_field], x[query_field]) for x in content])
+        return TrecQuery(qno_map)
 
     def __iter__(self) -> Iterator[Tuple[str, str]]:
         return iter(self._qno_map.items())
@@ -27,3 +31,11 @@ class TrecQuery:
 
     def __len__(self) -> int:
         return len(self._qno_map)
+
+    def head(self, n: int) -> TrecQuery:
+        qno_map = OrderedDict((k, v) for k, v in list(self._qno_map.items())[:n])
+        return TrecQuery(qno_map)
+
+    def tail(self, n: int) -> TrecQuery:
+        qno_map = OrderedDict((k, v) for k, v in list(self._qno_map.items())[n:])
+        return TrecQuery(qno_map)
