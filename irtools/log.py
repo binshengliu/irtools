132f18bd28bd2b912b0798fd5f9065e04c0cc4a6
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-07-08 14:31:43 +1000

    update get_logger wrapper

--- irtools/log.py
+++ irtools/log.py
@@ -1,21 +1,30 @@
-import os
 import logging
+import os
+import sys
+from typing import Optional, TextIO
 
 
-def get_logger(name, path=None):
+def get_logger(
+    name: str,
+    format: str = "[%(asctime)s][%(levelname)s] - %(message)s",
+    level: int = logging.INFO,
+    stream: Optional[TextIO] = sys.stderr,
+    path: Optional[str] = None,
+) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    logger.setLevel(level)
+    formatter = logging.Formatter(format)
 
     if path is not None:
         fh = logging.FileHandler(os.path.expanduser(path))
-        fh.setLevel(logging.DEBUG)
+        fh.setLevel(level)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    if stream is not None:
+        ch = logging.StreamHandler(stream)
+        ch.setLevel(level)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
 
     return logger
