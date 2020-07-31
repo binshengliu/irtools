c3858aa2345d08471a2ee5794a9f0059582c7693
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-07-31 17:27:45 +1000

    add default desc for tqdmf

--- irtools/tqdmf.py
+++ irtools/tqdmf.py
@@ -1,16 +1,20 @@
 from tqdm import tqdm
+from typing import Any, AnyStr, Iterator
 import os
 
 
-def tqdmf(path, *args, **kwargs):
+def tqdmf(path: os.PathLike[AnyStr], *args: Any, **kwargs: Any) -> Iterator[str]:
+    desc = kwargs.pop("desc", str(path).rsplit("/", maxsplit=1)[-1])
     with tqdm(
-            total=os.stat(path).st_size,
-            unit='B',
-            unit_scale=True,
-            unit_divisor=1024,
-            *args,
-            **kwargs) as bar:
-        with open(path, 'r') as f:
+        total=os.stat(path).st_size,
+        unit="B",
+        unit_scale=True,
+        unit_divisor=1024,
+        desc=desc,
+        *args,
+        **kwargs
+    ) as bar:
+        with open(path, "r") as f:
             for line in f:
                 bar.update(len(line))
                 yield line
