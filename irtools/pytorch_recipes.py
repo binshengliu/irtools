3e0497ceedfad52ed810309409daf523c9d341c5
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-11-17 20:34:20 +1100

    add masked_align to align jagged arrays

--- irtools/pytorch_recipes.py
+++ irtools/pytorch_recipes.py
@@ -32,3 +32,21 @@ def tensor_to_primitive(data: Union[torch.Tensor, Sequence[T], Mapping[Any, T]])
         return [tensor_to_primitive(x) for x in data]
     else:
         return data
+
+
+def masked_align(
+    tensor: torch.Tensor, mask: torch.Tensor, pad: Any = 0, keep_shape: bool = False
+) -> torch.Tensor:
+    in_mask = mask.bool()
+    length = in_mask.int().sum(dim=1)
+    out_mask: torch.Tensor = torch.arange(  # type: ignore
+        length.max(), device=tensor.device
+    )
+    out_mask = out_mask[None, :] < length[:, None]
+    if keep_shape:
+        new_shape = (*tensor.shape,)
+    else:
+        new_shape = (tensor.shape[0], length.max().item(), *tensor.shape[2:])
+    out = tensor.new_full(new_shape, pad)
+    out[out_mask] = tensor[in_mask]
+    return out
