118685b57726a52f491c18df5cdd075c12a077fc
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2021-08-31 20:03:19 +1000

    format

--- irtools/indri.py
+++ irtools/indri.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-from dask.distributed import Client
-from multiprocessing import Pool
-from more_itertools import unzip
-from itertools import repeat
-from pathlib import Path
-from lxml import etree
-from tqdm import tqdm
-import subprocess
 import argparse
-import tempfile
-import string
-import sys
 import os
+import string
+import subprocess
+import sys
+import tempfile
+from itertools import repeat
+from multiprocessing import Pool
+from pathlib import Path
+
+from dask.distributed import Client
+from lxml import etree
+from more_itertools import unzip
+from tqdm import tqdm
 
 
 def eprint(*args, **kwargs):
@@ -20,36 +21,36 @@ def eprint(*args, **kwargs):
 
 class IndriRunQuery:
     def __init__(self, path, index, scheduler=None):
-        self._path = path if path else 'IndriRunQuery'
+        self._path = path if path else "IndriRunQuery"
         self._index = index.strip()
         self._scheduler = scheduler
 
-    def format_xml(self, qno, query, working_set=[], extra=[]):
-        root = etree.Element('parameters')
+    def format_xml(self, qno, query, working_set=[], extra={}):
+        root = etree.Element("parameters")
 
-        node_format = etree.SubElement(root, 'trecFormat')
-        node_format.text = 'true'
+        node_format = etree.SubElement(root, "trecFormat")
+        node_format.text = "true"
 
-        node_index = etree.SubElement(root, 'index')
+        node_index = etree.SubElement(root, "index")
         node_index.text = self._index
 
-        for el in extra:
-            etree.SubElement(root, el[0]).text = str(el[1])
+        for k, v in extra.items():
+            etree.SubElement(root, k).text = str(v)
 
-        node_query = etree.SubElement(root, 'query')
+        node_query = etree.SubElement(root, "query")
 
-        etree.SubElement(node_query, 'number').text = qno
-        etree.SubElement(node_query, 'text').text = query
+        etree.SubElement(node_query, "number").text = qno
+        etree.SubElement(node_query, "text").text = query
 
         for docno in working_set:
-            etree.SubElement(node_query, 'workingSetDocno').text = docno
+            etree.SubElement(node_query, "workingSetDocno").text = docno
 
-        return etree.tostring(root, pretty_print=True).decode('ascii')
+        return etree.tostring(root, pretty_print=True).decode("ascii")
 
-    def run_file(self, qno, query, working_set, extra=[]):
-        string = self.format_xml('0', query, working_set, extra)
+    def run_file(self, qno, query, working_set, extra={}):
+        string = self.format_xml("0", query, working_set, extra)
 
-        fp = tempfile.NamedTemporaryFile(mode='w')
+        fp = tempfile.NamedTemporaryFile(mode="w")
         fp.write(string)
         fp.flush()
 
@@ -59,46 +60,51 @@ class IndriRunQuery:
             indri_args,
             stdout=subprocess.PIPE,
             stderr=subprocess.DEVNULL,
-            encoding='ascii',
-            errors='ignore')
+            encoding="ascii",
+            errors="ignore",
+        )
         output = []
         for line in proc.stdout:
-            if 'EXCEPTION' in line:
-                eprint('EXCEPTION: {} {} {}'.format(qno, query, line))
-                raise Exception('EXCEPTION: {} {} {}'.format(qno, query, line))
-            line = ' '.join([qno] + line.split(maxsplit=1)[1:])
+            if "EXCEPTION" in line:
+                eprint("EXCEPTION: {} {} {}".format(qno, query, line))
+                raise Exception("EXCEPTION: {} {} {}".format(qno, query, line))
+            line = " ".join([qno] + line.split(maxsplit=1)[1:])
             output.append(line)
 
         fp.close()
 
-        output = ''.join(output)
+        output = "".join(output)
         return output
 
-    def run_cmd(self, qno, query, extra=[]):
+    def run_cmd(self, qno, query, extra={}):
         indri_args = [
-            self._path, '-index=' + self._index, '-trecFormat=True',
-            '-queryOffset=0', '-query={}'.format(query)
+            self._path,
+            "-index=" + self._index,
+            "-trecFormat=True",
+            "-queryOffset=0",
+            "-query={}".format(query),
         ]
 
         if extra:
-            extra = ['-{}={}'.format(*el) for el in extra]
+            extra = [f"-{k}={v}" for k, v in extra.items()]
             indri_args.extend(extra)
 
         proc = subprocess.Popen(
             indri_args,
             stdout=subprocess.PIPE,
             stderr=subprocess.DEVNULL,
-            encoding='ascii',
-            errors='ignore')
+            encoding="ascii",
+            errors="ignore",
+        )
         output = []
         for line in proc.stdout:
-            if 'EXCEPTION' in line:
-                eprint('EXCEPTION: {} {} {}'.format(qno, query, line))
-                raise Exception('EXCEPTION: {} {} {}'.format(qno, query, line))
-            line = ' '.join([qno] + line.split(maxsplit=1)[1:])
+            if "EXCEPTION" in line:
+                eprint("EXCEPTION: {} {} {}".format(qno, query, line))
+                raise Exception("EXCEPTION: {} {} {}".format(qno, query, line))
+            line = " ".join([qno] + line.split(maxsplit=1)[1:])
             output.append(line)
 
-        output = ''.join(output)
+        output = "".join(output)
         return output
 
     def run_single(self, packed_args):
@@ -110,55 +116,58 @@ class IndriRunQuery:
 
         return output
 
-    def run_batch(self, qnos, queries, working_set, extra, workers):
+    def run_batch(self, qnos, queries, working_set=None, extra={}, workers=1):
+        if working_set is None:
+            working_set = repeat(None)
         with Pool(workers) as pool:
             results = pool.imap(
-                self.run_single,
-                zip(qnos, queries, repeat(working_set), repeat(extra)))
+                self.run_single, zip(qnos, queries, working_set, repeat(extra))
+            )
             results = list(tqdm(results, total=len(qnos)))
         return results
 
-    def run_distributed(self, qnos, queries, working_set=[], extra=[]):
-        '''Set up a cluster first:
+    def run_distributed(self, qnos, queries, working_set=[], extra={}):
+        """Set up a cluster first:
         dask-scheduler
         env PYTHONPATH=/research/remote/petabyte/users/binsheng/trec_tools/ dask-worker segsresap10:8786 --nprocs 50 --nthreads 1 --memory-limit 0 --name segsresap10
         env PYTHONPATH=/research/remote/petabyte/users/binsheng/trec_tools/ dask-worker segsresap10:8786 --nprocs 50 --nthreads 1 --memory-limit 0 --name segsresap09
-        '''
+        """
         client = Client(self._scheduler)
         futures = client.map(
-            self.run_single,
-            zip(qnos, queries, repeat(working_set), repeat(extra)))
+            self.run_single, zip(qnos, queries, repeat(working_set), repeat(extra))
+        )
         output = [f.result() for f in futures]
         return output
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description="")
 
-    parser.add_argument('--index', required=True, type=Path, help='Index')
+    parser.add_argument("--index", required=True, type=Path, help="Index")
 
     parser.add_argument(
-        '--query',
+        "--query",
         required=True,
-        help='Query: csv file, or \'-\' for stdin. Format: qno,query',
-        type=argparse.FileType('r'))
+        help="Query: csv file, or '-' for stdin. Format: qno,query",
+        type=argparse.FileType("r"),
+    )
 
     parser.add_argument(
-        '--sep',
-        choices=[',', 'space', 'none'],
-        default=',',
-        help='Separator between qno and query')
+        "--sep",
+        choices=[",", "space", "none"],
+        default=",",
+        help="Separator between qno and query",
+    )
 
-    parser.add_argument('--scheduler', help='Scheduler')
+    parser.add_argument("--scheduler", help="Scheduler")
 
-    parser.add_argument(
-        '--count', default=1000, type=int, help='Document count')
-
-    parser.add_argument(
-        '--workers', default=os.cpu_count() // 2, type=int, help='Workers')
+    parser.add_argument("--count", default=1000, type=int, help="Document count")
 
     parser.add_argument(
-        '--output', default=sys.stdout, type=argparse.FileType('w'))
+        "--workers", default=os.cpu_count() // 2, type=int, help="Workers"
+    )
+
+    parser.add_argument("--output", default=sys.stdout, type=argparse.FileType("w"))
 
     return parser.parse_args()
 
@@ -167,14 +176,14 @@ def main():
     args = parse_arguments()
     content = [x.strip() for x in args.query if x.strip()]
 
-    if args.sep != 'none':
-        sep = None if args.sep == 'space' else args.sep
+    if args.sep != "none":
+        sep = None if args.sep == "space" else args.sep
         qnos, queries = unzip(line.split(sep, maxsplit=1) for line in content)
     else:
         queries = content
         qnos = list(map(str, range(len(queries))))
 
-    trans = str.maketrans('', '', string.punctuation)
+    trans = str.maketrans("", "", string.punctuation)
     queries = [s.translate(trans) for s in queries]
 
     qnos = list(qnos)
@@ -183,18 +192,18 @@ def main():
     indri = IndriRunQuery(None, str(args.index.resolve()), args.scheduler)
 
     if args.scheduler:
-        output = indri.run_distributed(
-            qnos, queries, extra=[['count', args.count]])
+        output = indri.run_distributed(qnos, queries, extra={"count": args.count})
     else:
         output = indri.run_batch(
             qnos,
             queries,
             working_set=[],
-            extra=[['count', args.count]],
-            workers=args.workers)
+            extra={"count": args.count},
+            workers=args.workers,
+        )
 
     args.output.writelines(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
