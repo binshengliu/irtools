a00c78609be34fc9dc6c6bec8c8312b711676c04
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2022-12-30 10:37:32 +1100

    update tqdmf

--- irtools/tqdmf.py
+++ irtools/tqdmf.py
@@ -1,25 +1,26 @@
 import gzip
 import os
 import struct
-from typing import Any, AnyStr, Iterator
+from typing import Any, Iterator, Union
 
 from tqdm import tqdm
 
-
-def getuncompressedsize(filename: "os.PathLike[AnyStr]") -> int:
-    with open(filename, 'rb') as f:
-        f.seek(-4, 2)
-        return struct.unpack('I', f.read(4))[0]
+AnyPath = Union[str, bytes, os.PathLike[str], os.PathLike[bytes]]
 
 
-def tqdmf(path: "os.PathLike[AnyStr]", *args: Any, **kwargs: Any) -> Iterator[str]:
+def tqdmf(path: AnyPath, *args: Any, **kwargs: Any) -> Iterator[str]:
+    def getuncompressedsize(filename: AnyPath) -> int:
+        with open(filename, 'rb') as f:
+            f.seek(-4, 2)
+            return struct.unpack('I', f.read(4))[0]  # type: ignore
+
     desc = kwargs.pop("desc", str(path).rsplit("/", maxsplit=1)[-1])
     if str(path).endswith(".gz"):
         openf = gzip.open
         total = getuncompressedsize(path)
         mode = "rt"
     else:
-        openf = open
+        openf = open            # type: ignore
         total = os.stat(path).st_size
         mode = "r"
     with tqdm(
@@ -37,4 +38,4 @@ def tqdmf(path: "os.PathLike[AnyStr]", *args: Any, **kwargs: Any) -> Iterator[st
                     bar.total = bar.total + 4 * (1024 ** 3)
                     bar.refresh()
                 bar.update(len(line))
-                yield line
+                yield line      # type: ignore
