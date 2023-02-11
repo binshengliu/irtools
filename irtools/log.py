73236e69e9dee8ac90878cfc3d4119493826d421
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2023-02-11 11:30:41 +1100

    update log

--- irtools/log.py
+++ irtools/log.py
@@ -1,30 +1,21 @@
 import logging
 import os
-import sys
-from typing import Optional, TextIO
+from typing import Optional
+
+color_fmt = "[%(asctime)s][%(name)s][%(log_color)s%(levelname)s%(reset)s] - %(message)s"
+log_fmt = "[%(asctime)s][%(name)s][%(levelname)s] - %(message)s"
 
 
-def get_logger(
-    name: str,
-    format: str = "[%(asctime)s][%(name)s][%(levelname)s] - %(message)s",
-    level: int = logging.INFO,
-    stream: Optional[TextIO] = sys.stderr,
-    path: Optional[str] = None,
-) -> logging.Logger:
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    formatter = logging.Formatter(format)
+def configure_logging(log_level: str = "INFO", path: Optional[str] = None) -> None:
+    level = logging.getLevelName(log_level)
+    try:
+        import colorlog
+
+        colorlog.basicConfig(level=level, format=color_fmt)
+    except Exception:
+        logging.basicConfig(level=level, format=log_fmt)
 
     if path is not None:
         fh = logging.FileHandler(os.path.expanduser(path))
-        fh.setLevel(level)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-
-    if stream is not None:
-        ch = logging.StreamHandler(stream)
-        ch.setLevel(level)
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
-
-    return logger
+        fh.setFormatter(logging.Formatter(log_fmt))
+        logging.root.addHandler(fh)
